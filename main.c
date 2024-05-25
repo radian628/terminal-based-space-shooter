f2cb194d0784c6dfd9c0b26ac25cc731e275d173
radian628 <70352880+radian628@users.noreply.github.com>
2024-05-24 20:08:30 -0700

    janky TUI input garbage

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
 
 int main() {
-  printf("Hello world!\n");
+  // printf("Hello world!\n");
+  char test[4] = "abcd";
+  struct termios mode;
+
+  tcgetattr(0, &mode);
+  mode.c_lflag &= ~(ECHO | ICANON);
+  tcsetattr(0, TCSANOW, &mode);
+
+  for (int i = 0; i < 4; i++) {
+    read(0, test + i, 1);
+  }
+  printf("\ngot string: %s\n", test);
+
+  mode.c_lflag |= ECHO | ICANON;
+  tcsetattr(0, TCSANOW, &mode);
 }
