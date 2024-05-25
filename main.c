ae645d4c4516af12722a89334cec6cd1172134cb
radian628 <70352880+radian628@users.noreply.github.com>
2024-05-24 20:18:12 -0700

    added terminal clear stuff
    we can now clear the terminal!!!!!!!

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
+#include <string.h>
+#include <fcntl.h>
 
 int main() {
   // printf("Hello world!\n");
   char test[4] = "abcd";
   struct termios mode;
+
+  // ansi escape seq to clear the terminal (ignore keyboard spam at the end)
+  printf("\e[1;1H\e[2J asdffasdfasdasfasdd");
+  // int tty_file = open("/dev/tty", O_RDWR, 0);
+  const char *msg = "haha stupid terminal get overwritten!";
+  write(1, msg, strlen(msg));
+  fflush(stdout);
 
   tcgetattr(0, &mode);
   mode.c_lflag &= ~(ECHO | ICANON);
