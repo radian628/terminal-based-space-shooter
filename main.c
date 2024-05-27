896c88ce3c7560b9f778c4e62552db58edbf0860
vvill <contact@vvill.dev>
2024-05-27 00:24:22 -0700

    Added first attempt at display code

--- main.c
+++ main.c
@@ -1,80 +1,32 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
-#include <string.h>
 #include <fcntl.h>
 #include <stdlib.h>
 
 #include "dynarray.h"
 #include "input.h"
+#include "display.h"
 
-void printScreen(int min_width, int min_height) {
-  printf("\x1B[1;1H\x1B[2J");
-  fflush(stdout);
-  struct winsize w;
-  ioctl(0, TIOCGWINSZ, &w);
-  if(w.ws_col < min_width || w.ws_row < min_height) {
-    char msg[1024];
-    sprintf(msg, "Term (%d x %d) Too Small", w.ws_col, w.ws_row);
-    for(int i = 0; i < w.ws_col; i++) {
-      printf("#");
-    }
-    for(int i = 0; i < w.ws_row/2-1; i++) {
-      printf("\n#");
-      for(int j = 0; j < w.ws_col-2; j++) {
-        printf(" ");
-      }
-      printf("#");
-    }
-    printf("\n#");
-    for(int i = 0; i < w.ws_col/2 -1- (strlen(msg)/2); i++) {
-      printf(" ");
-    }
-    printf("%s", msg);
-    for(int i = 0; i < w.ws_col/2 -2- (strlen(msg)/2); i++) {
-      printf(" ");
-    }
-    printf("#");
-    for(int i = 0; i < w.ws_row/2-1; i++) {
-      printf("\n#");
-      for(int j = 0; j < w.ws_col-2; j++) {
-        printf(" ");
-      }
-      printf("#");
-    }
-    printf("\n");
-    for(int i = 0; i < w.ws_col; i++) {
-      printf("#");
-    }
-   }
-  fflush(stdout);
-}
 
 int main() {
-
-  // ansi escape seq to clear the terminal (ignore keyboard spam at the end)
-  printf("\x1B[1;1H\x1B[2J");
-  const char *msg = "haha stupid terminal get overwritten!";
-  write(1, msg, strlen(msg));
-  fflush(stdout);
-  printScreen(80, 30);
-
   disable_echo_and_canonical();
+  init_screen();
   // repeatedly poll for input
   while (1) {
+    print_screen(80, 30);
+    sleep(1);
 
     // get all input from stdin
-    dynarray *input = get_all_of_stdin();
+    //dynarray *input = get_all_of_stdin();
 
     // write input to stdout if any exists
-    if (da_size(input))
-      write(1, da_start(input), da_size(input));
+    //if (da_size(input))
+      //write(1, da_start(input), da_size(input));
 
     // exit if user presses "x"
-    if (((char *)da_start(input))[0] == 'x') {
-      break;
-    }
-    free(input);
+    //if (((char *)da_start(input))[0] == 'x') {
+      //break;
+    //}
+    //free(input);
   }
 
   enable_echo_and_canonical();
