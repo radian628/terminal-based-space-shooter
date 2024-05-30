40cbafea74114f3c223396147df46b6c9fde3aaf
radian628 <70352880+radian628@users.noreply.github.com>
2024-05-29 18:18:03 -0700

    got "uncooked terminal" working

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
+const fs = std.fs;
+const os = std.os.linux;
+const debug = std.debug;
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
@@ -14,6 +17,33 @@ pub fn main() !void {
     try stdout.print("Run `zig build test` to run the tests.\n", .{});
 
     try bw.flush(); // don't forget to flush!
+
+    var tty: fs.File = try fs.cwd().openFile("/dev/tty", .{ .mode = .read_write });
+    defer tty.close();
+
+    var old_termios: os.termios = undefined;
+    _ = os.tcgetattr(tty.handle, &old_termios);
+    var modified_termios = old_termios;
+
+    // modified_termios.lflag &= ~@as(os.tc_lflag_t, .os.tc_lflag_t.ECHO | .os.tc_lflag_t.ICANON);
+    modified_termios.lflag.ECHO = false;
+    modified_termios.lflag.ICANON = false;
+
+    _ = os.tcsetattr(tty.handle, .FLUSH, &modified_termios);
+
+    while (true) {
+        var buffer: [1]u8 = undefined;
+
+        _ = try tty.read(&buffer);
+
+        if (buffer[0] == 'x') {
+            break;
+        }
+
+        try tty.writer().print("got input: {s}\n", .{buffer});
+    }
+
+    _ = os.tcsetattr(tty.handle, .FLUSH, &old_termios);
 }
 
 test "simple test" {
