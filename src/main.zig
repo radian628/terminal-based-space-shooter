902faae180e7b8d268ab50cab68a6c27430b2288
radian628 <70352880+radian628@users.noreply.github.com>
2024-05-29 20:16:24 -0700

    removed extra junk

--- src/main.zig
+++ src/main.zig
@@ -4,51 +4,35 @@ const os = std.os.linux;
 const debug = std.debug;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-
+    // get current terminal
     var tty: fs.File = try fs.cwd().openFile("/dev/tty", .{ .mode = .read_write });
     defer tty.close();
 
+    // get termios object
     var old_termios: os.termios = undefined;
     _ = os.tcgetattr(tty.handle, &old_termios);
     var modified_termios = old_termios;
 
-    // modified_termios.lflag &= ~@as(os.tc_lflag_t, .os.tc_lflag_t.ECHO | .os.tc_lflag_t.ICANON);
+    // disable echo and icanon
     modified_termios.lflag.ECHO = false;
     modified_termios.lflag.ICANON = false;
-
     _ = os.tcsetattr(tty.handle, .FLUSH, &modified_termios);
 
+    // repeatedly get chars from input
     while (true) {
+        // get input
         var buffer: [1]u8 = undefined;
-
         _ = try tty.read(&buffer);
 
+        // exit if input is 'x'
         if (buffer[0] == 'x') {
             break;
         }
 
+        // acknowledge the input; print it to console
         try tty.writer().print("got input: {s}\n", .{buffer});
     }
 
+    // re-enable echo and icanon
     _ = os.tcsetattr(tty.handle, .FLUSH, &old_termios);
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
